fabe216359f45bd3f3f3831f986404e2adf6f6e6
IlCarretto <carlorebuffat412@gmail.com>
2022-10-09 22:36:07 +0200

    working on inputs

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 // 0.21 € al km
 // 20% minorenni
 // 40% over 65
+// Aggiungere +20 euro se medium class, +40 se first class, altrimenti 0
 
 // Input dell'utente
 
@@ -13,10 +14,14 @@ console.log(kmTravelledInput, typeof(kmTravelledInput));
 const userAgeInput = (document.getElementById("user-age"));
 console.log(userAgeInput, typeof(userAgeInput));
 
+// Chiedere classe
+const userClassInput = document.getElementById("user-class")
+console.log(userClassInput);
+
 const submitBtn = document.getElementById("submit");
 console.log(submitBtn);
-const title = document.querySelector("h1");
-console.log(title);
+const title = document.getElementById("final-price");
+console.log(title, typeof(title));
 
 // Click sul bottone
 submitBtn.addEventListener("click", function () {
@@ -25,6 +30,7 @@ submitBtn.addEventListener("click", function () {
     console.log(kmTravelled, typeof(kmTravelled));
     const userAge = userAgeInput.value;
     console.log(userAge, typeof(userAge));
+    const userClass = userClassInput.value;
 
     // Converto i km in euro
     const kmPrice = kmTravelled * 0.21;
@@ -41,11 +47,31 @@ submitBtn.addEventListener("click", function () {
         ticketPrice = kmPrice;
     }
 
+    // Aggiungere +20 euro se medium class, +40 se first class, altrimenti 0
+
     ticketPrice = Math.round(ticketPrice * 100) / 100;
 
     // Inserisco il valore in h1
-    title.innerHTML = `Il prezzo del tuo biglietto è ${ticketPrice}!`
+    title.innerHTML = `IMPORTO TOTALE PAGATO ${ticketPrice}!`
     console.log(title.innerHTML);
+
+    // Inserisco il posto casuale assegnato
+    let value = Math.floor(Math.random() * 200) +1;
+    console.log(value);
+
+    const trainSpot = document.querySelector(".train-spot");
+    console.log(trainSpot);
+
+    trainSpot.innerHTML = `${value}`
+
+    // Inserisco i punti freccia casuali
+    let value2 = Math.floor(Math.random() * 1000) +1;
+    console.log(value);
+
+    const trainPoints = document.querySelector(".train-points");
+    console.log(trainPoints);
+
+    trainPoints.innerHTML = `${value}`
 
     // Ripulisco gli input
     kmTravelledInput.value = "";
